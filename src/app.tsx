4b75623ad06ba42d797ea6f0899e4807e72408cb
Hu Tianyv <1394139263@qq.com>
2024-07-21 12:52:43 +0800

    feat: add nutui

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,7 @@
 import { PropsWithChildren } from 'react';
 import { useLaunch } from '@tarojs/taro';
 import { View } from '@tarojs/components';
+import '@nutui/nutui-taro/dist/style.css';
 import './app.scss';
 
 function App(props: PropsWithChildren<any>){
